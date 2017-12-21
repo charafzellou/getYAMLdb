af53d9be0dec6b991d1e4324f9832990903e048f
evilflash <evilflash@zik.dj>
2017-12-21 14:13:55 +0100

    V0.0.2.2 - Got the team on GitHub :p
    YAY!

--- yamlParser.c
+++ yamlParser.c
@@ -1,36 +1,46 @@
 #include "yamlParser.h"
 
 int YAML_Parser() {
+    //  Initiate all the vars in the table
     FILE* file = fopen("DataBases/db1/users.yaml", "rb+");
     char* stream = malloc(sizeof(char)*2048);
     short pointeur = ftell(file);
     char actual_char;
 
-    // Look if file exists
-    if(file == NULL)
-    {
-        fprintf(stderr,"\nERROR, \ncannot open the weather file");
-        return -1;
-    }
-
-    // Initiate all the vars in the table
-    int counter;
+    short counter;
     float number;
     char average;
     char* humidity;
     char c_counter[256], c_number[256], c_average[256], c_humidity[256];
 
-    // Set the cursor at beginning
-    fseek(file, 0, SEEK_SET);
+    //  Look if file exists
+    if(file == NULL)
+    {
+        fprintf(stderr,"\nERROR, cannot open the weather file");
+        return -1;
+    }
 
-    // read line by line
+    //  Storing COUNTER - how many records are in the table
     fseek(file, 9, SEEK_SET);
-    short i = 0;
-    while(actual_char != '\n'){
+    actual_char = fgetc(file);
+    if(actual_char != '\n')
+        counter = (int)actual_char;
+    printf("The selected table contains %c records to show:\n", counter);
+
+    //  Saut jusqu'au USER1
+    short seekJump = 7;
+    while(seekJump > 0){
         actual_char = fgetc(file);
-        printf("%c", actual_char);
-        c_counter[i] = actual_char;
-        i++;
+        if(actual_char == '\n'){
+            seekJump--;
+        }
+    }
+
+    seekJump = 5;
+    while(seekJump > 0){
+        fgets(stream, 128, file);
+        printf("%s", stream);
+        seekJump--;
     }
 
     fclose(file);
