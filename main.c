f4a24eb5ee564ed69311c3bfc0b6b4f95325d699
juanProject <chrunch4life@gmail.com>
2017-12-22 01:22:25 +0100

    V0.0.1
    ajout du menu

--- main.c
+++ main.c
@@ -11,8 +11,38 @@
 #include "yamlParser.h"
 
 int main() {
-    //YAML_Parser();
-    menu1();
+    char name[255];
+    char choiceMenu1;
+
+    do{
+        printf("----   MENU   ----\n");
+        printf("a.   partie 1\n");
+        printf("b.   partie 2\n");
+        printf("c.   partie 3\n");
+        printf("q.   quitter\n");
+        scanf("%c", &choiceMenu1);
+        fflush(stdin);
+        system("cls");
+        switch(choiceMenu1){
+            case 'a':
+
+                break;
+            case 'b':
+                menu1();
+                break;
+            case 'c':
+                YAML_Parser();
+                break;
+            case 'q':
+                system("cls");
+                printf("Bye");
+                break;
+            default:
+                printf("option inconnue :/\n");
+                Sleep(1000);
+                system("cls");
+        }
+    }while ( choiceMenu1 != 'q' );
     //  Create Database
     //  Create Table
     //  Create Record
